614a8fe279f804f3a99e507ed0762e3d11f8ec9e
htmlgod <ivannevtrilov212@gmail.com>
2023-09-08 00:52:23 +0300

    template with boost::program_options

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,38 @@
-#include <iostream>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
-auto main(int argc, char** argv) -> int {
-    std::cout << "hello, world" << std::endl;
+#include <iostream>
+#include <iterator>
+
+int main(int argc, char** argv)
+{
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("print-hello", "produce hello-world message")
+        ;
+        po::variables_map vm;        
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);    
+
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
+            return 0;
+        }
+
+        if (vm.count("print-hello")) {
+            std::cout << "Hello, world!\n";
+            return 0;
+        }
+    }
+    catch(std::exception& e) {
+        std::cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        std::cerr << "Exception of unknown type!\n";
+    }
 
     return 0;
 }
